3310724b8f2d2f0bdd64a9ee19d78befb3a05b3c
Phani2799 <156699491+Phani2799@users.noreply.github.com>
2024-01-15 22:37:14 -0600

    Update problem2.py
    The replace() method is applied to the sentence variable.it searches for the substring python in the sentence and replace it with pythons.
    
    the updated sentence is then stored in the variable modifed_sentence.

--- problem2.py
+++ problem2.py
@@ -1,3 +1,3 @@
 sentence = input("Enter a sentence: ")
 modified_sentence = sentence.replace('python', 'pythons')
-print("Modified Sentence:", modified_sentence)
+ print("Modified Sentence:", modified_sentence)
